47a13fbdf958e31c01f3eabf0b31420a78289d09
haybe <haybe@EWDOD>
2023-08-22 14:53:30 +0300

    Add project files.

---
+++ Services/WeatherComService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces;
+
+namespace Services
+{
+    public class WeatherComService : IWeatherService
+    {
+        public decimal GetTemperature(string yer)
+        {
+            yer = yer.ToLower();
+
+            switch (yer)
+            {
+                case "ankara":
+                    return 33m;
+                case "istanbul":
+                    return 34m;
+                case "adana":
+                    return 38m;
+                case "tokyo":
+                    return 26;
+                default:
+                    return new Random().Next(5, 35);
+            }
+        }
+
+        public decimal Temperature(string place)
+        {
+            return GetTemperature(place);
+        }
+    }
+}
---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWeatherService _weatherService;
+
+        public HomeController(ILogger<HomeController> logger, IWeatherService weatherService)
+        {
+            _logger = logger;
+            _weatherService = weatherService;
+        }
+
+        public IActionResult Index(string? yer)
+        {
+            if (!string.IsNullOrEmpty(yer))
+            {
+                decimal sicaklik = _weatherService.Temperature(yer);
+                ViewBag.Sonuc = $"{yer} için hava sıcaklığı: {sicaklik}°C";
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
